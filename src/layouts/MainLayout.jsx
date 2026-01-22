7505324a7ec1ecb59751cdb3f860228ba2cd1f0f
olgavegh <olgi.vegh@gmail.com>
2026-01-22 17:55:52 +0100

    Phase 4: Layout & Core Components - MainLayout comppnent

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+function MainLayout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default MainLayout
